0c86239ae98e879d2b8c3b46f1f86354a0688e60
wangjunjun <wangjun5132@gmail.com>
2019-08-11 23:36:01 +0800

    fixed: change

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 
+import '../static/scss/app.scss';
 export interface IAppProps {
 }
 
 export default class App extends React.Component<IAppProps> {
   public render() {
     return (
-      <div>
+      <div className='m-app'>
         hello zhongg
       </div>
     );
